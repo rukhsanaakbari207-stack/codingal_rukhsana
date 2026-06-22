3291038415d87dbc3a9ae5c79461d514a219eb45
Rukhsana <rukhsanaakbari207@gmail.com>
2026-06-21 22:24:52 -0700

    activityhomework2.py

---
+++ lesson4/activityhomework2.py
@@ -0,0 +1,11 @@
+birthday1 = "jun 3"
+birthday2 = "2000"
+birthday3 = "may 19"
+birthday4 = "march 7"
+birthday5 = "january 12"
+
+print(birthday1)
+print(birthday2)
+print(birthday3)
+print(birthday4)
+print(birthday5)
